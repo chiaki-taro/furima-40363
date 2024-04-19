791979b102d4f2635c9733a1ccef4967306c1664
chiaki-taro <chias.926@gmail.com>
2024-04-20 06:50:13 +0900

    itemモデルの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :name,           presence: true
+  validates :explanation,    presence: true
+  validates :category_id,    presence: true
+  validates :condition_id,   presence: true
+  validates :postage_id,     presence: true
+  validates :prefecture_id,  presence: true
+  validates :preparation_id, presence: true
+  validates :price,          presence: true
+  validates :image,          presence: true
 end
