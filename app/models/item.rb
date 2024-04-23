c92c85dc105b0930cd0b9ef7718d873436a1470f
chiaki-taro <chias.926@gmail.com>
2024-04-23 23:49:18 +0900

    出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,33 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparation
+
   belongs_to :user
   has_one_attached :image
 
-  validates :name,           presence: true
-  validates :explanation,    presence: true
-  validates :category_id,    presence: true
-  validates :condition_id,   presence: true
-  validates :postage_id,     presence: true
-  validates :prefecture_id,  presence: true
-  validates :preparation_id, presence: true
-  validates :price,          presence: true
-  validates :image,          presence: true
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :preparation_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :image
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :preparation_id
+  end
+
 end
