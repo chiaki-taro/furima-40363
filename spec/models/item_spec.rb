d725cffcd9838efb0f260fc106a8999a27b6810d
chiaki-taro <chias.926@gmail.com>
2024-04-24 22:15:04 +0900

    PR修正3

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,9 +38,10 @@ RSpec.describe Item, type: :model do
 
     context '出品ができないとき' do
       it 'ユーザー登録している人でないと出品できない' do
-        @item.user_id = nil
+        @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include
+        binding.pry
+        expect(@item.errors.full_messages).to include("User must exist")
       end
       it '画像がないと出品できない' do
         @item.image = nil
