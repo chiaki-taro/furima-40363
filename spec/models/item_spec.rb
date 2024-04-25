acbcf2eee122b3931c56b1e63e38440114dccfaa
chiaki-taro <chias.926@gmail.com>
2024-04-26 07:27:45 +0900

    Rubocopを実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Item, type: :model do
       it 'ユーザー登録している人でないと出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it '画像がないと出品できない' do
         @item.image = nil
@@ -123,9 +123,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it '販売価格に半角数字以外が入力されていたら出品できない' do
-        @item.price = "ああああ"
+        @item.price = 'ああああ'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
