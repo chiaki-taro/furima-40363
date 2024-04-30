d84e78a30b88b30a99800f82f64a6f2e37d5326e
chiaki-taro <chias.926@gmail.com>
2024-04-30 17:16:23 +0900

    購入画面への遷移,実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @order = Order.new
+    @item = Item.find(params[:item_id])
+    @order_adress = OrderAddress.new
   end
 
   def create
