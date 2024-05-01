1ef99e2c7e3a3d6106b2a2c6d9edb71f3dd22567
chiaki-taro <chias.926@gmail.com>
2024-05-01 22:42:55 +0900

    エラー文表示の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,3 +1,21 @@
 class OrderAddress
   include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id
+    validates :item_id
+    # addressモデルのバリデーション
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
+  end
 end
